258693a10d0e02b1ba39e5e32f4ef25e5234f8b1
Darshan L <darshanl1254@gmail.com>
2024-12-17 01:25:34 +0530

    Rest Api for products

---
+++ src/test/java/com/dars/product_api/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dars.product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
